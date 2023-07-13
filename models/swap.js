fa6913d9b876b7962f301d7f30e90d7179ebd16f
alan-cousin <bluesoft.alan@gmail.com>
2023-07-13 09:25:33 +0900

    fix to js

---
+++ models/swap.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const SwapSchema = new mongoose.Schema({
+    discordId: {
+        type: String,
+        required: true,
+      },
+    walletAddress: {
+        type: String,
+        required: true
+    },
+    tokenPair: {
+        type: String,
+        required: true
+    },
+    tokenAddress: {
+        type: String,
+        required: true
+    },
+    limitPrice: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose?.model('Swap', SwapSchema);
