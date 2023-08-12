ee821dcd913986d0fefebf41486589c1ff35ecb2
alan-cousin <bluesoft.alan@gmail.com>
2023-08-12 11:53:54 +0900

    fix

--- libs/user.js
+++ libs/user.js
@@ -996,7 +996,7 @@ class ASAPUser {
 						.setTitle(`${symbol}/WETH`)
 						.setDescription(symbol + "\n`" + tokenAdress + "`")
 						.addFields(
-							{ name: 'Created', value: `<t:${tradeAt.getTime() / 1000}:R>`, inline: false }
+							{ name: 'Created', value: tradeAt, inline: false }
 						)
 						.addFields(
 							{ 
